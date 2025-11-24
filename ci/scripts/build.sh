d24935c5d9b1e77d2af1246a0152ac8b0593c266
willtemperley <willtemperley@gmail.com>
2025-11-24 15:06:26 +0800

    chore: Move ArrowTests to standard layout (#109)
    ## What's Changed
    
    ArrowTests has been moved to the standard package layout. Generated test
    file paths have been updated.
    
    Contributes toward closing #78.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -53,12 +53,12 @@ export GOPATH="${build_dir}"
 pushd "${data_gen_dir}"
 go get -d ./...
 go run .
-cp *.arrow ../../Arrow
+cp *.arrow ../../Tests/ArrowTests
 popd
 github_actions_group_end
 
 github_actions_group_begin "Use -warnings-as-errors"
-for package in . Arrow ArrowFlight; do
+for package in . ArrowFlight; do
   pushd "${build_dir}/source/${package}"
   sed 's/\/\/ build://g' Package.swift > Package.swift.build
   mv Package.swift.build Package.swift
